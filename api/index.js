58bb57c6f64362b755fc2ce092f40ad0e51f9c67
Darwin <d.hernan1007@gmail.com>
2023-07-14 09:24:54 -0500

    second commit

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ const routerApi = require('./routes')
 const {logErrors, errorHandler, boomErrorHandler} = require('./middlewares/errorsHandler')
 
 const app = express();
-const port = porcess.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 
 app.use(express.json())
